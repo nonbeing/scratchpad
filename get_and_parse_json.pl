6eab680fd1c5f6482744baf594a4c685a120d90c
Ambar Seksena <ambar.seksena@inmobi.com>
2013-03-15 21:42:39 +0530

    Simple script to get a json response from the Google books API and parse it in perl. Will use this as a building block in my koha extension.

---
+++ get_and_parse_json.pl
@@ -0,0 +1,63 @@
+#!/usr/bin/perl -w
+
+# based on idea from http://beerpla.net/2008/03/27/parsing-json-in-perl-by-example-southparkstudioscom-south-park-episodes/
+ 
+use strict;
+use WWW::Mechanize;
+use JSON -support_by_pp;
+ 
+ 
+sub fetch_json_page
+{
+    my ($json_url) = @_;
+    my $browser = WWW::Mechanize->new();
+    
+    eval
+    {
+        # download the json page:
+        print "Opening URL: $json_url\n";
+        $browser->get( $json_url );
+        my $content = $browser->content();
+
+        # create new JSON parser
+        my $json = new JSON;
+     
+        # these are some nice json options to relax restrictions a bit:
+        my $json_text = $json->allow_nonref->utf8->relaxed->escape_slash->loose->allow_singlequote->allow_barekey->decode($content);
+
+        # json_text now contains the decoded JSON
+     
+        # iterate over each element in the items[] array:
+        my $item_number = 1;
+        foreach my $item(@{$json_text->{items}})
+        {
+            my %item_hash = ();
+            $item_hash{number} = "Item $item_number";
+            $item_hash{title} = $item->{volumeInfo}->{title};
+
+            # ToDo: join the elements of the authors array with commas into a string, then put that into $item_hash{authors}
+            # $item_hash{authors} = $item->{description};
+
+            $item_hash{thumbnail_url} = $item->{volumeInfo}->{imageLinks}->{thumbnail};
+            $item_hash{publisher} = $item->{volumeInfo}->{publisher};
+            $item_hash{pageCount} = $item->{volumeInfo}->{pageCount};
+     
+            # print item information:
+            print "Found Item:\n";
+            while (my($k, $v) = each (%item_hash))
+            {
+                print "$k => $v\n";
+            }
+            print "\n";
+     
+            $item_number++;
+        }
+    };
+    # catch crashes:
+    if($@){
+        print "[[JSON ERROR]] JSON parser crashed! $@\n";
+    }
+}
+
+fetch_json_page("https://www.googleapis.com/books/v1/volumes?q=isbn:8179923703");
+
