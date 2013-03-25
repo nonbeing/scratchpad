294209ee60aba6b38f01450d0b1ba3165c67aa24
Ambar Seksena <ambar.seksena@gmail.com>
2013-03-25 14:02:47 +0530

    Various fixes to script

--- koha/temp/koha_db_backup_nightly.sh
+++ koha/temp/koha_db_backup_nightly.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+# do NOT run this script as root!
 
-backupDir=/home/ambar/libraryDBBackups
-backupLogfile=${HOME}/backup.last.status.log
+# ensure that the user running this script has done "s3cmd --configure" with access and secret keys,
+# otherwise this script won't work
+
+
+
+backupDir=${HOME}/libraryDBBackups
+backupLogfile=/var/log/backup.last.status.log
 now=`date +%a.%d_%m_%Y.%H-%M`
 
-cd $backupDir
+if [[ -e ${backupDir} ]]
+then
+    cd ${backupDir}
+else
+    mkdir ${backupDir}
+    cd ${backupDir}
+fi
 
 
 # take the mysql DB backup
@@ -16,15 +28,20 @@ mysqldump -u root -pJGDkohaAOL13may koha_library2 | bzip2 > koha_library2.${now}
 lastBackup=`ls -1 koha_library2*.bz2 | tail -1`
 
 # upload the DB backup to S3
-s3cmd put  ${lastBackup}  s3://kohalibrarydata
+s3cmd put  ${lastBackup}  s3://kohalibrarydata | tee -a ${backupLogfile}
 
 
 if [[ `s3cmd ls s3://kohalibrarydata | grep "${lastBackup}"` ]]
 then
     echo "[DONE] [${now}] Successfully backed up koha_library2 DB and pushed it to S3" | tee -a ${backupLogfile}
-    rm -rf $backupDir/*
+    if [[ -e ${backupDir} ]]
+    then
+        rm -rf ${backupDir}/*
+    else
+        echo "[ERROR] ${backupDir} does not exist!!!" | tee -a ${backupLogfile}
+    fi
 else
-    echo "[ERROR] [${now}] Did not find ${lastBackup} on S3... please investigate" | tee -a ${backup}
+    echo "[ERROR] [${now}] Did not find ${lastBackup} on S3... please investigate" | tee -a ${backupLogfile}
 fi    
 
 
